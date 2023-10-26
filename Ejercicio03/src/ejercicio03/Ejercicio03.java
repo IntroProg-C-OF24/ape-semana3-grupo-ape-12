ee2c870698016227a9e6f1ab4f32d16479f48f16
Jhon Alejandro <jaquinde6@utpl.edu.ec>
2023-10-26 18:11:50 -0500

    Ejercicio03/Este comentario es de Alejandro

--- Ejercicio03/src/ejercicio03/Ejercicio03.java
+++ Ejercicio03/src/ejercicio03/Ejercicio03.java
@@ -1,22 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejercicio03;
-
-/**
- *
- * @author reroes
- */
 public class Ejercicio03 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-        
+        double var1 = Math.pow(10, 2);
+        double var2 = Math.sqrt(16);
+        double var3 = 20 / (1+11);
+        System.out.println("El resultado es: " + (var1+var2+var3));
     }
     
 }
