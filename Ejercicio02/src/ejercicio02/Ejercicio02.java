e676f2ee4cb882764d41556749b7b633beea298d
Manuel Gomez <magomez36@utpl.edu.ec>
2023-10-30 17:30:12 -0500

    Ejercicio02 resuelto por Manuel Gomez

--- Ejercicio02/src/ejercicio02/Ejercicio02.java
+++ Ejercicio02/src/ejercicio02/Ejercicio02.java
@@ -7,7 +7,7 @@ public class Ejercicio02 {
         double respuesta;
         respuesta = Math.pow(var1,2) + var2;
         System.out.println("La respuesta es: " + respuesta);
-        //Autor Manuel Gomez
+        //Autor Manuel Gomez.
                 
     }
     
