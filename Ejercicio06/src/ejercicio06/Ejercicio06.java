950f38006ee34d1c8882de833e51e208ed64c2eb
Manuel Gomez <magomez36@utpl.edu.ec>
2023-10-30 18:17:44 -0500

    Ejercicio06 resuelto por Manuel Gomez

--- Ejercicio06/src/ejercicio06/Ejercicio06.java
+++ Ejercicio06/src/ejercicio06/Ejercicio06.java
@@ -1,20 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package ejercicio06;
-
-/**
- *
- * @author sandr
- */
 public class Ejercicio06 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Resolucion del ejercicio
+        int var1 = 81;
+        int var2 = 9;
+        int var3 = 3;
+        int var4 = 9;
+        int var5 = 10;
+        int var6 = 1;
+        boolean respuesta;
+        respuesta = (Math.sqrt(var1) + (var2) / (var3) == (var4) && (var5) > (var6));
+        System.out.println("La respuesta es: " + respuesta);
+        //Autor Manuel Gomez.
+      
     }
     
 }
