129bfaa514e9cb1441d291e04dc7fb57985357f1
Manuel Gomez <magomez36@utpl.edu.ec>
2023-10-30 23:58:57 -0500

    Ejercicio10 resuelto por Manuel Gomez

--- Ejercicio10/src/ejercicio10/Ejercicio10.java
+++ Ejercicio10/src/ejercicio10/Ejercicio10.java
@@ -1,20 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package ejercicio10;
-
-/**
- *
- * @author sandr
- */
 public class Ejercicio10 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Resolución del Ejercicio
+        int var1 = 25;
+        int var2 = 10;
+        int var3 = 100;
+        int var4 = 10;
+        int var5 = 5;
+        int var6 = 2;
+        boolean respuesta;
+        respuesta = (Math.sqrt(var1)*var2 >= var3) && (true) || (false) || ((var4 / var5) >= var6);
+        System.out.println("La respuesta es: " + respuesta);
+        //Autor Manuel Gomez.
     }
     
 }
