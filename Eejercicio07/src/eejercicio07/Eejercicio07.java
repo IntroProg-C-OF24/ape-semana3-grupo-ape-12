23b9ede9de574907805757cb72d801c134c1bb8c
Alejandro Quinde <jaquinde6utpl.edu.ec>
2023-10-31 18:03:09 -0500

    Este comentario es de Alejandro

---
+++ Eejercicio07/src/eejercicio07/Eejercicio07.java
@@ -0,0 +1,16 @@
+package eejercicio07;
+public class Eejercicio07 {
+
+    public static void main(String[] args) {
+        double var1 = Math.sqrt(81);
+        double var2 = 9 / (3);
+        int var3 = 10;
+        int var4 =1;
+        double var5 = 100 / (25);
+        double var6 = Math.sqrt(100);
+        System.out.println("El resultado es: " + var1+var2);
+        System.out.println("El numero es mayor es: " + Math.max(var3, var4));
+        System.out.println("segundo resultado es: " + var5+var6);
+    }
+    
+}
