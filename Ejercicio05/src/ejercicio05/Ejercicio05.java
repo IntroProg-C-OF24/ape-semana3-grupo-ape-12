9ccf98972ae36f3672e79b2027bd9fa47c6c0e76
Alejandro Quinde <jaquinde6utpl.edu.ec>
2023-10-31 14:58:16 -0500

    Este comentario es de Alejandro

---
+++ Ejercicio05/src/ejercicio05/Ejercicio05.java
@@ -0,0 +1,10 @@
+package ejercicio05;
+public class Ejercicio05 {
+
+    public static void main(String[] args) {
+        double var1 = Math.pow(2, 2);
+        int var2 = 12-1 / (3);
+        System.out.println("El resultado es: " + (var1+var2));
+    }
+    
+}
